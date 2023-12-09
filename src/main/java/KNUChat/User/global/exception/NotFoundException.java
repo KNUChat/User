1a4b5cd1225aac571e2ce02f0713c9ea473074e1
HyeonJeong <wgwjh05169@gmail.com>
2023-12-10 02:57:28 +0900

    refactor: 사용하지 않는 필드 삭제

--- src/main/java/KNUChat/User/global/exception/NotFoundException.java
+++ src/main/java/KNUChat/User/global/exception/NotFoundException.java
@@ -3,8 +3,6 @@ package KNUChat.User.global.exception;
 import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends KnuchatException {
-    private String source;
-
     public NotFoundException(String source) {
         super(HttpStatus.NOT_FOUND, "존재하지 않는 " + source + "입니다.");
     }
