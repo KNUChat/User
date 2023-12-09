767489eda755d68e24eabdabdf6eeb191f25ab14
HyeonJeong <wgwjh05169@gmail.com>
2023-12-10 02:25:44 +0900

    refactor: 사용하지 않는 라이브러리 삭제

--- src/main/java/KNUChat/User/global/exception/KnuchatException.java
+++ src/main/java/KNUChat/User/global/exception/KnuchatException.java
@@ -2,6 +2,7 @@ package KNUChat.User.global.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
@@ -9,5 +10,7 @@ import org.springframework.http.HttpStatus;
 public class KnuchatException extends RuntimeException{
 
     private final HttpStatus httpStatus;
+
+    @Setter
     private final String message;
 }
