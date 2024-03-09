bb40170b36fa384e870982cbdfae8f9f7df5b6c9
HyeonJeong <wgwjh05169@gmail.com>
2024-03-10 00:12:23 +0900

    comment: 불필요한 주석 제거

--- src/main/java/KNUChat/User/kafka/config/KafkaConfig.java
+++ src/main/java/KNUChat/User/kafka/config/KafkaConfig.java
@@ -29,7 +29,6 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        //Default
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(props);
